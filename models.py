634a2ecd824aaea7163298379fbf7447c0667a88
Nelestya <tristand.pro@gmail.com>
2017-10-12 18:33:32 +0200

    use an abstract class for the recente update an creation

--- models.py
+++ models.py
@@ -1,25 +1,10 @@
 from django.db import models
 
-# Create your models here.
 
-class Langue(models.Model):
-    name = models.CharField(max_length=5, primary_key=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Application(models.Model):
-    name = models.CharField(max_length=30, primary_key=True)
-    description = models.CharField(max_length=100)
-    author = models.CharField(max_length=30)
+#Abstract class
+class Recently(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    priority = models.IntegerField()
-
-
-    def __str__(self):
-        return self.name
 
     def update_recent(self):
         """
@@ -33,19 +18,32 @@ class Application(models.Model):
         """
         return (datetime.now() - self.created).days < 30
 
-class ApiConnect(models.Model):
-    name = models.CharField(max_length=30)
-    description = models.CharField(max_length=50)
-    code = models.TextField()
+    class Meta:
+        abstract = True
+
+# Create your models here.
+class Langue(models.Model):
+    name = models.CharField(max_length=5, primary_key=True)
 
     def __str__(self):
         return self.name
 
-class Image(models.Model):
+
+class Application(Recently):
+    name = models.CharField(max_length=30, primary_key=True)
+    description = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
+    priority = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class Image(Recently):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=70)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
     langue = models.ForeignKey(
              'Langue',
@@ -57,41 +55,13 @@ class Image(models.Model):
            'SectionImage',
            on_delete=models.CASCADE,)
 
-
-
-    def update_recent(self):
-        """
-        return True if is updated recently
-        """
-        return (datetime.now() - self.updated).days < 30
-
-    def created_recent(self):
-        """
-        return True if is created recently
-        """
-        return (datetime.now() - self.created).days < 30
-
     def __str__(self):
         return self.name
 
 
 
-class SectionImage(models.Model):
+class SectionImage(Recently):
     name = models.CharField(max_length=50, primary_key=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def update_recent(self):
-        """
-        return True if is updated recently
-        """
-        return (datetime.now() - self.updated).days < 30
-
-    def created_recent(self):
-        """
-        return True if is created recently
-        """
-        return (datetime.now() - self.created).days < 30
 
     def __str__(self):
         return self.name
